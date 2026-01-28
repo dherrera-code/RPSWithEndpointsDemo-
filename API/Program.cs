5079f26a63f96f1b8f40874c052605ef8db6602b
dherrera-code <dherr929@gmail.com>
2026-01-28 10:42:42 -0800

    resolving cors!

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("Client", policy =>
     {   //Links to your front-end to grant permission to access the API!, Can specify your local or hosted websites!
-        policy.WithOrigins("http://localhost:5500","http://127.0.0.1:5500","https://wonderful-stone-0eb28261e.4.azurestaticapps.net/")
+        policy.WithOrigins("http://localhost:5500","http://127.0.0.1:5500","https://wonderful-stone-0eb28261e.4.azurestaticapps.net")
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
@@ -21,7 +21,6 @@ builder.Services.AddCors(options =>
 // "Client" is refering toward the name of the frontEnd you need to have access to the API!
 
 var app = builder.Build();
-app.UseCors("Client");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,6 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("Client");
 
 app.UseAuthorization();
 
